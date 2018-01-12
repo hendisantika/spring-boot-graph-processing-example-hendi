7eb28f670fe5a3df3ac933cc25029f481ef81eab
Hendi Santika <hendisantika@gmail.com>
2018-01-12 07:26:39 +0700

    #12 - Add  unit test - Config

--- config/src/test/java/com/hendisantika/config/ConfigApplicationTests.java
+++ config/src/test/java/com/hendisantika/config/ConfigApplicationTests.java
@@ -2,15 +2,43 @@ package com.hendisantika.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class ConfigApplicationTests {
 
+    @Value("${local.server.port}")
+    private int port = 0;
+
     @Test
-    public void contextLoads() {
+    public void configurationAvailable() {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
+                "http://localhost:" + port + "/app/production", Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+
+    @Test
+    public void envPostAvailable() {
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = new TestRestTemplate().postForEntity(
+                "http://localhost:" + port + "/admin/env", form, Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
     }
 
 }
