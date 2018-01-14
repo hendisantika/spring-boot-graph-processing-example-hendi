f7ba18efbf25bd115939e1a4299450ccfbe910ab
Hendi Santika <hendisantika@gmail.com>
2018-01-14 07:28:50 +0700

    #15 - Add  Initial Project - Twitter Rank web

---
+++ twitter-rank-web/src/main/java/com/hendisantika/twitterrankweb/TwitterRankWebApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.twitterrankweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterRankWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterRankWebApplication.class, args);
+    }
+}
