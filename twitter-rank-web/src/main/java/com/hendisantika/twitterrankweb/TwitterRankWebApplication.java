ed96a9eb8471ff0a2bd6fbfedc48ebee84e9f58f
Hendi Santika <hendisantika@gmail.com>
2018-01-14 07:32:09 +0700

    #16 - Modify Main App - Twitter Rank web

--- twitter-rank-web/src/main/java/com/hendisantika/twitterrankweb/TwitterRankWebApplication.java
+++ twitter-rank-web/src/main/java/com/hendisantika/twitterrankweb/TwitterRankWebApplication.java
@@ -2,8 +2,12 @@ package com.hendisantika.twitterrankweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@SpringCloudApplication
+@EnableZuulProxy
 public class TwitterRankWebApplication {
 
     public static void main(String[] args) {
