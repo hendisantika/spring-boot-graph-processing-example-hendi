b4d750daf1ca75480e0bebe69c6b40c759fa6404
Hendi Santika <hendisantika@gmail.com>
2018-01-24 22:47:37 +0700

    #20 - Initial Commit - Twitter Rank Crawler

---
+++ twitter-rank-crawler/src/main/java/com/hendisantika/twitterrankcrawler/TwitterRankCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.twitterrankcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterRankCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterRankCrawlerApplication.class, args);
+    }
+}
