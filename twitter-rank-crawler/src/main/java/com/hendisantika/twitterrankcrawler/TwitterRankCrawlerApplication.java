dcf961f74a921ccbb30a1187351e8ec319bd14ce
Hendi Santika <hendisantika@gmail.com>
2018-01-24 22:53:08 +0700

    #22 - Modify Main App

--- twitter-rank-crawler/src/main/java/com/hendisantika/twitterrankcrawler/TwitterRankCrawlerApplication.java
+++ twitter-rank-crawler/src/main/java/com/hendisantika/twitterrankcrawler/TwitterRankCrawlerApplication.java
@@ -1,12 +1,18 @@
 package com.hendisantika.twitterrankcrawler;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-public class TwitterRankCrawlerApplication {
+@SpringCloudApplication
+@EnableZuulProxy
+@EnableScheduling
+public class TwitterRankCrawlerApplication extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) {
-        SpringApplication.run(TwitterRankCrawlerApplication.class, args);
+        new SpringApplicationBuilder(TwitterRankCrawlerApplication.class).web(true).run(args);
     }
 }
